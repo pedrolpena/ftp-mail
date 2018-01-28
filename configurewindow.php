d404eabd1ef757e1ab1fa7fcf7a7ccecdc588ca3
Pedro Pena <pedro.pena@noaa.gov>
2018-01-28 17:52:51 -0500

    starting to add config page

---
+++ configurewindow.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <h2>This is where the configuration page will go</h2>
+    </body>
+
+</html>
--- index.php
+++ index.php
@@ -7,13 +7,15 @@
 <body>
 
     <table id="mainTable">
-
         <tr>
             <td id="tabRowa" colspan="3">
-                <ul id="headerList">
-                    <li><a href="messagelist.php" target="messageIframe">Inbox</a></li><li><a href="outboxlist.php" target="messageIframe">Outbox</a></li><li><a href="sentlist.php" target="messageIframe">Sent</a></li><li><a href="smswindow.php" target="messageIframe">SMS</a></li>
+                <ul id="headerList"><!--
+                    --><li><a href="messagelist.php" target="messageIframe">Inbox</a></li><!--
+                    --><li><a href="outboxlist.php" target="messageIframe">Outbox</a></li><!--
+                    --><li><a href="sentlist.php" target="messageIframe">Sent</a></li><!--
+                    --><li><a href="smswindow.php" target="messageIframe">SMS</a></li>
                     </ul>
-</td>
+            </td>
         <tr >
             <td id="header1" colspan="3">
                AOML FTP Mail
@@ -24,9 +26,13 @@
             <td id="headerRowa">
                 <table id="tableLeft">
                     <tr>
-                        <td><br><a href="composewindow.php?reply=no" target="messageIframe">Compose</a></td>
-                    </tr>
-
+                        <td>
+							<ul id="leftList">
+							    <li><a href="composewindow.php?reply=no" target="messageIframe">Compose</a></li><!--
+							    --><li><a href="configurewindow.php?reply=no" target="messageIframe">Configure</a></li>
+						    </ul>
+					    </td>
+                    </tr>  
                 </table>
             </td>
             <td id="headerRowb">
